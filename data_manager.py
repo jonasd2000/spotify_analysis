54b0c926d9b5dd1c0a6fcfc4ab3022e77baca8c2
Jonas Dupre <jonasdupre@yahoo.de>
2024-01-26 21:17:46 +0100

    simple data implementation implemented

--- data_manager.py
+++ data_manager.py
@@ -3,13 +3,82 @@ import os
 
 import polars as pl
 
+class GroupByAggregateParser:
+    aggregate_choices = [
+        "count",
+        "sum",
+        "min",
+        "max",
+        "mean",
+    ]
+    
+    group_by: str
+    aggregate_function: str
+    aggregate_by: str
+    
+    def __init__(self) -> None:
+        self.group_by = ""
+        self.aggregate_function = ""
+        self.aggregate_by = ""
+        
+    def set_group_by(self, group_by: str) -> None:
+        self.group_by = group_by
+        
+    def process_group_by_change_event(self, event) -> None:
+        self.set_group_by(event.value)
+        
+    def get_group_by_expression(self) -> pl.Expr:
+        if not self.group_by:
+            return None
+        return pl.col(self.group_by)
+        
+    def set_aggregate_function(self, aggregate_function: str) -> None:
+        self.aggregate_function = aggregate_function
+        
+    def process_aggregate_function_change_event(self, event) -> None:
+        self.set_aggregate_function(event.value)
+        
+    def set_aggregate_by(self, aggregate_by: str) -> None:
+        self.aggregate_by = aggregate_by
+        
+    def process_aggregate_by_change_event(self, event) -> None:
+        self.set_aggregate_by(event.value)
+
+    def get_aggregate_expression(self) -> pl.Expr:
+        # aggregate functions that don't need a column
+        match self.aggregate_function:
+            case "count":
+                return pl.count()
+            case _:
+                pass
+        
+        if self.aggregate_by == "":
+            return None
+        
+        # aggregate functions that need a column to aggregate
+        match self.aggregate_function:
+            case "sum":
+                return pl.col(self.aggregate_by).sum()
+            case "min":
+                return pl.col(self.aggregate_by).min()
+            case "max":
+                return pl.col(self.aggregate_by).max()
+            case "mean":
+                return pl.col(self.aggregate_by).mean()
+            case _:
+                return None
+        
+
 AUDIO_STREAMING_HISTORY_FILENAME_START = 'Streaming_History_Audio'
 
 class DataManager:
+    group_by_aggregate_parser: GroupByAggregateParser
+    
     path: Path
     full_data: pl.DataFrame
     
     def __init__(self) -> None:
+        self.group_by_aggregate_parser = GroupByAggregateParser()
         self.path = Path()
     
     def is_audio_streaming_history_file(self, path: Path, filename: str) -> bool:
@@ -33,4 +102,18 @@ class DataManager:
         dataframes = [self.read_audio_streaming_file(path / filename) for filename in file_paths]
         
         self.full_data = pl.concat(dataframes)
+        
+    def get_data(self) -> pl.DataFrame:
+        group_by_expression = self.group_by_aggregate_parser.get_group_by_expression()
+        aggregate_expression = self.group_by_aggregate_parser.get_aggregate_expression()
+        
+        if group_by_expression is None or aggregate_expression is None:
+            print("Group by or aggregate function not set")
+            return self.full_data
+        
+        print(f"Group by: {group_by_expression}, aggregate by: {aggregate_expression}")
+        return self.full_data\
+                .group_by(self.group_by_aggregate_parser.get_group_by_expression())\
+                .agg(self.group_by_aggregate_parser.get_aggregate_expression())#\
+                # .sort(pl.col(sort_by))
         
--- ui_manager.py
+++ ui_manager.py
@@ -1,16 +1,40 @@
-from nicegui import ui
+from nicegui import ui, element
+import polars as pl
 
-from data_manager import DataManager
+from data_manager import DataManager, GroupByAggregateParser
 
 class UIManager:
     data_manager: DataManager
     
+    data_table: element.Element
+    
     def __init__(self, data_manager: DataManager) -> None:
         self.data_manager = data_manager
+        self.data_table = None
+    
+    def create_data_table(self, dataframe: pl.DataFrame) -> None:
+        columns = [
+            {'name': column, 'label': column.capitalize(), 'field': column, 'sortable': True}
+            for column in dataframe.columns
+        ]
+        rows = dataframe.to_dicts()
+        return ui.table(columns=columns, rows=rows, pagination=100)
     
     def create_ui(self) -> None:
         ui.label(str(self.data_manager.path))
-        ui.button('BUTTON', on_click=lambda: ui.notify('button was pressed'))
+        
+        with ui.row():
+            ui.select(self.data_manager.full_data.columns, label="Group by", multiple=True, on_change=self.data_manager.group_by_aggregate_parser.process_group_by_change_event)
+            ui.select(self.data_manager.group_by_aggregate_parser.aggregate_choices, label="Aggregate function", on_change=self.data_manager.group_by_aggregate_parser.process_aggregate_function_change_event)
+            ui.select(self.data_manager.full_data.columns, label="Aggregate by", on_change=self.data_manager.group_by_aggregate_parser.process_aggregate_by_change_event)
+            
+        def on_submit():
+            if self.data_table is not None:
+                self.data_table.delete()
+            self.data_table = self.create_data_table(self.data_manager.get_data())
+        
+        ui.button("Submit", on_click=on_submit)
+        
     
     def run(self) -> None:
         self.create_ui()
