7e546c50382e6fa8efd1deaf52f1b4cbf4a5b2e9
Jonas Dupre <jonasdupre@yahoo.de>
2024-06-04 14:02:29 +0200

    show raw data when no group by or aggregate is set

--- data_manager.py
+++ data_manager.py
@@ -205,6 +205,9 @@ class DataManager:
         if group_by_expression is None or aggregate_expression is None:
             print("Group by or aggregate function not set")
             return df
+        if not group_by_expression.meta.is_column() or not aggregate_expression.meta.is_column():
+            print("Group by or aggregate function not a column")
+            return df
         
         print(f"Group by: {group_by_expression}, aggregate by: {aggregate_expression}")
         df = df.group_by(self.group_by_aggregate_parser.get_group_by_expression())\
