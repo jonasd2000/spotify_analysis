4a8a90a1bfb77894c5c2f820b48ab3974705b380
Jonas Dupre <jonasdupre@yahoo.de>
2025-06-20 17:57:40 +0200

    data as charts

--- ui/pages/analysis_home.py
+++ ui/pages/analysis_home.py
@@ -11,8 +11,19 @@ class AnalysisHome(Page):
             .agg(pl.sum('ms_played'))\
             .sort('ms_played', descending=True).limit(10)
             
-        rows = most_listened_tracks.to_dicts()
-        ui.table(rows=rows)
+        track_names = most_listened_tracks['master_metadata_track_name'].to_list()
+        # artist_names = most_listened_tracks['master_metadata_album_artist_name'].to_list()
+        
+        # display_text = [f"{artist_names[i]} - {track_names[i]}" for i in range(len(track_names))]
+        
+        ms_played = most_listened_tracks['ms_played'].to_list()
+        
+        ui.echart({
+            'xAxis': {'type': 'category', 'data': track_names},
+            'yAxis': {'type': 'value'},
+            'series': [{'type': 'bar', 'data': ms_played}],
+            'tooltip': {'formatter': "{b}"},
+        })
     
     def create_artist_analysis_section(self):
         most_listened_artists = self.data_manager.streaming_data\
@@ -20,8 +31,15 @@ class AnalysisHome(Page):
             .agg(pl.sum('ms_played'))\
             .sort('ms_played', descending=True).limit(10)
             
-        rows = most_listened_artists.to_dicts()
-        ui.table(rows=rows)
+        artist_names = most_listened_artists['master_metadata_album_artist_name'].to_list()
+        ms_played = most_listened_artists['ms_played'].to_list()
+        
+        ui.echart({
+            'xAxis': {'type': 'category', 'data': artist_names},
+            'yAxis': {'type': 'value'},
+            'series': [{'type': 'bar', 'data': ms_played}],
+            'tooltip': {'formatter': "{b}"},
+        })
     
     def create_page(self, *args, **kwargs) -> None:
         with ui.row():
