1a0a07585196f157ebe4b7ef527d012bcc5a50df
Jonas Dupre <jonasdupre@yahoo.de>
2025-06-20 18:42:01 +0200

    categorisation of podcast episodes

--- ui/pages/analysis_home.py
+++ ui/pages/analysis_home.py
@@ -7,6 +7,7 @@ from .page import Page
 class AnalysisHome(Page):
     def create_track_analysis_section(self):
         most_listened_tracks = self.data_manager.streaming_data\
+            .filter(pl.col('media_type') == 'track')\
             .group_by('master_metadata_track_name')\
             .agg(pl.sum('ms_played'))\
             .sort('ms_played', descending=True).limit(10)
@@ -27,6 +28,7 @@ class AnalysisHome(Page):
     
     def create_artist_analysis_section(self):
         most_listened_artists = self.data_manager.streaming_data\
+            .filter(pl.col('media_type') == 'track')\
             .group_by('master_metadata_album_artist_name')\
             .agg(pl.sum('ms_played'))\
             .sort('ms_played', descending=True).limit(10)
