06c06de670ecc276a20221565cc59b4c17469e5b
Jonas Dupre <jonasdupre@yahoo.de>
2025-06-20 22:53:01 +0200

    charts display hours played

--- ui/pages/analysis_home.py
+++ ui/pages/analysis_home.py
@@ -27,7 +27,7 @@ class AnalysisHome(Page):
             'text': x,
             'textposition': 'inside',
             'insidetextanchor': 'start',
-            'hovertemplate': '<b>%{y}</b><br>%{x} ms played<extra></extra>',
+            'hovertemplate': '<b>%{y}</b><br>%{x} hours played<extra></extra>',
         }
     
     def get_top(self, feature, media_type, limit=10):
@@ -36,7 +36,7 @@ class AnalysisHome(Page):
             .group_by(feature)\
             .agg(pl.sum('ms_played'))\
             .sort('ms_played', descending=True).limit(limit)\
-            .with_columns(pl.duration(milliseconds=pl.col('ms_played')).alias('duration')).sort('ms_played', descending=False)
+            .sort('ms_played', descending=False)
     
     def create_track_analysis_chart(self):
         most_listened_tracks = self.get_top(('master_metadata_track_name', 'master_metadata_album_artist_name'), 'track')
@@ -45,10 +45,10 @@ class AnalysisHome(Page):
         artist_names = most_listened_tracks['master_metadata_album_artist_name'].to_list()
         display_text = [f"{track_name} - {artist_name}" for track_name, artist_name in zip(track_names, artist_names)]
         
-        ms_played = most_listened_tracks['ms_played'].to_list()
+        hours_played = (most_listened_tracks['ms_played'] / 1000 / 3600).to_list()
         fig = {
             'data': [
-                self._get_chart_data(x=track_names, y=ms_played, text=display_text),
+                self._get_chart_data(x=track_names, y=hours_played, text=display_text),
             ],
             'layout': self.top_graph_layouts,
             'config': self.top_graph_config,
@@ -60,11 +60,11 @@ class AnalysisHome(Page):
         most_listened_artists = self.get_top('master_metadata_album_artist_name', 'track')
             
         artist_names = most_listened_artists['master_metadata_album_artist_name'].to_list()
-        ms_played = most_listened_artists['ms_played'].to_list()
+        hours_played = (most_listened_artists['ms_played'] / 1000 / 3600).to_list()
         
         fig = {
             'data': [
-                self._get_chart_data(x=artist_names, y=ms_played, text=artist_names),
+                self._get_chart_data(x=artist_names, y=hours_played, text=artist_names),
             ],
             'layout': self.top_graph_layouts,
             'config': self.top_graph_config,
@@ -76,11 +76,11 @@ class AnalysisHome(Page):
         most_listened_podcasts = self.get_top('episode_show_name', 'episode')
             
         podcast_names = most_listened_podcasts['episode_show_name'].to_list()
-        ms_played = most_listened_podcasts['ms_played'].to_list()
+        hours_played = (most_listened_podcasts['ms_played'] / 1000 / 3600).to_list()
         
         fig = {
             'data': [
-                self._get_chart_data(x=podcast_names, y=ms_played, text=podcast_names),
+                self._get_chart_data(x=podcast_names, y=hours_played, text=podcast_names),
             ],
             'layout': self.top_graph_layouts,
             'config': self.top_graph_config,
@@ -90,7 +90,8 @@ class AnalysisHome(Page):
     
     def create_track_analysis_section(self):
         unique_tracks = self.data_manager.streaming_data.select(pl.col('master_metadata_track_name')).to_series().drop_nulls().unique().len()
-        total_time = self.data_manager.streaming_data.filter(pl.col('media_type') == 'track').with_columns(pl.duration(milliseconds=pl.col('ms_played')).alias('duration')).select(pl.col('duration')).to_series().drop_nulls().sum()
+        total_time = self.data_manager.streaming_data.filter(pl.col('media_type') == 'track')\
+            .with_columns(pl.duration(milliseconds=pl.col('ms_played')).alias('duration')).select(pl.col('duration')).to_series().drop_nulls().sum()
         with ui.row():
             self.create_track_analysis_chart()
             with ui.column():
@@ -106,7 +107,8 @@ class AnalysisHome(Page):
             
     def create_podcast_analysis_section(self):
         unique_podcasts = self.data_manager.streaming_data.select(pl.col('episode_show_name')).to_series().drop_nulls().unique().len()
-        total_time = self.data_manager.streaming_data.filter(pl.col('media_type') == 'episode').with_columns(pl.duration(milliseconds=pl.col('ms_played')).alias('duration')).select(pl.col('duration')).to_series().drop_nulls().sum()
+        total_time = self.data_manager.streaming_data.filter(pl.col('media_type') == 'episode')\
+            .with_columns(pl.duration(milliseconds=pl.col('ms_played')).alias('duration')).select(pl.col('duration')).to_series().drop_nulls().sum()
         with ui.row():
             self.create_podcast_analysis_chart()
             with ui.column():
