09ff9eaab563a9babb915c771241358869aba5b3
Jonas Dupre <jonasdupre@yahoo.de>
2025-06-21 13:30:58 +0200

    customdata for plotly charts

--- ui/pages/analysis_home.py
+++ ui/pages/analysis_home.py
@@ -23,8 +23,8 @@ class AnalysisHome(Page):
             'name': 'Top Tracks',
             'orientation': 'h',
             'x': y,
-            'y': text,
-            'text': x,
+            'y': x,
+            'text': text,
             'textposition': 'inside',
             'insidetextanchor': 'start',
             'hovertemplate': '<b>%{y}</b><br>%{x} hours played<extra></extra>',
@@ -36,19 +36,26 @@ class AnalysisHome(Page):
             .group_by(feature)\
             .agg(pl.sum('ms_played'))\
             .sort('ms_played', descending=True).limit(limit)\
-            .sort('ms_played', descending=False)
+            .sort('ms_played', descending=False).with_columns(pl.duration(milliseconds=pl.col('ms_played')).alias('duration'))
     
     def create_track_analysis_chart(self):
         most_listened_tracks = self.get_top(('master_metadata_track_name', 'master_metadata_album_artist_name'), 'track')
         
         track_names = most_listened_tracks['master_metadata_track_name'].to_list()
         artist_names = most_listened_tracks['master_metadata_album_artist_name'].to_list()
-        display_text = [f"{track_name} - {artist_name}" for track_name, artist_name in zip(track_names, artist_names)]
         
-        hours_played = (most_listened_tracks['ms_played'] / 1000 / 3600).to_list()
+        durations = most_listened_tracks['duration']
+        hours_played = (durations.dt.total_seconds() / 3600).to_list()
+        
+        trace = self._get_chart_data(x=track_names, y=hours_played, text=track_names)
+        trace.update(
+            customdata=[(an, humanize.precisedelta(d, format="%0.0f")) for an, d in zip(artist_names, durations)],
+            hovertemplate=r"%<b>{text}</b> - %{customdata[0]}<br><extra>Played for %{customdata[1]}</extra>",
+        )
+        
         fig = {
             'data': [
-                self._get_chart_data(x=track_names, y=hours_played, text=display_text),
+                trace,
             ],
             'layout': self.top_graph_layouts,
             'config': self.top_graph_config,
@@ -60,11 +67,19 @@ class AnalysisHome(Page):
         most_listened_artists = self.get_top('master_metadata_album_artist_name', 'track')
             
         artist_names = most_listened_artists['master_metadata_album_artist_name'].to_list()
-        hours_played = (most_listened_artists['ms_played'] / 1000 / 3600).to_list()
+        
+        durations = most_listened_artists['duration']
+        hours_played = (durations.dt.total_seconds() / 3600).to_list()
+        
+        trace = self._get_chart_data(x=artist_names, y=hours_played, text=artist_names)
+        trace.update(
+            customdata=[humanize.precisedelta(d, format="%0.0f") for d in durations],
+            hovertemplate=r"%{text}<br><extra>Played for %{customdata}</extra>",
+        )
         
         fig = {
             'data': [
-                self._get_chart_data(x=artist_names, y=hours_played, text=artist_names),
+                trace,
             ],
             'layout': self.top_graph_layouts,
             'config': self.top_graph_config,
@@ -74,13 +89,21 @@ class AnalysisHome(Page):
     
     def create_podcast_analysis_chart(self):
         most_listened_podcasts = self.get_top('episode_show_name', 'episode')
-            
+        
         podcast_names = most_listened_podcasts['episode_show_name'].to_list()
-        hours_played = (most_listened_podcasts['ms_played'] / 1000 / 3600).to_list()
+        
+        durations = most_listened_podcasts['duration']
+        hours_played = (durations.dt.total_seconds() / 3600).to_list()
+        
+        trace = self._get_chart_data(x=podcast_names, y=hours_played, text=podcast_names)
+        trace.update(
+            customdata=[humanize.precisedelta(d, format="%0.0f") for d in durations],
+            hovertemplate=r"%{text}<br><extra>Played for %{customdata}</extra>",
+        )
         
         fig = {
             'data': [
-                self._get_chart_data(x=podcast_names, y=hours_played, text=podcast_names),
+                trace,
             ],
             'layout': self.top_graph_layouts,
             'config': self.top_graph_config,
