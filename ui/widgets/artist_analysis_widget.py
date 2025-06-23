3a6b60c7396e1a2e419fdff3c08fa2e22967b2a8
Jonas Dupré <jonasdupre@yahoo.de>
2025-06-23 19:15:08 +0200

    create artist_over_time plot framework

--- ui/widgets/artist_analysis_widget.py
+++ ui/widgets/artist_analysis_widget.py
@@ -4,6 +4,22 @@ from .widget import Widget
 
 
 class ArtistAnalysisWidget(Widget):
+    top_graph_layouts = {
+        "plot_bgcolor": "#E5ECF6",
+        "xaxis": {"fixedrange": True, "showticklabels": False},
+        "yaxis": {
+            "fixedrange": True,
+            "gridcolor": "white",
+            "title": {"text": "Hours Played"},
+        },
+    }
+    top_graph_config = {
+        "responsive": True,
+        "displayModeBar": False,
+    }
+
+    selected_artist = None
+
     def on_event(self, name, *args, propagate=True, **kwargs):
         match name:
             case "data_change":
@@ -25,9 +41,37 @@ class ArtistAnalysisWidget(Widget):
             .to_list()
         )
 
-    def on_artist_change(self, event):
-        artist = event.value
-        ui.notify(f"Selected artist: {artist}")
+    def on_artist_change_forward(self, select_artist: str) -> str:
+        """
+        Called when the artist_select widget is changed.
+        Sets the value of self.selected_artist.
+        """
+        return select_artist
+
+    def on_artist_change_backward(self, self_artist: str) -> str:
+        """
+        Called when the selected_artist attribute of this widget is changed.
+        Sets the value of the self.artist_select widget.
+        """
+        return self_artist
+
+    def create_artist_over_time_trace(self):
+        if self.data_manager.streaming_data.is_empty():
+            return None
+
+    def create_artist_over_time_plot(self):
+        trace = self.create_artist_over_time_trace()
+        fig = {
+            "data": [
+                trace,
+            ],
+            "layout": self.top_graph_layouts,
+            "config": self.top_graph_config,
+        }
+
+        self.artist_over_time_chart = ui.plotly(fig)
+
+        return self.artist_over_time_chart
 
     def create_widget(self, *args, **kwargs):
         with ui.column() as widget:
@@ -35,6 +79,12 @@ class ArtistAnalysisWidget(Widget):
                 self.get_artist_names(),
                 label="Artist",
                 with_input=True,
-                on_change=self.on_artist_change,
+            ).bind_value(
+                self,
+                "selected_artist",
+                forward=self.on_artist_change_forward,
+                backward=self.on_artist_change_backward,
             )
+            with ui.grid(rows=1, columns=r"100%").classes("w-dvw"):
+                self.create_artist_over_time_plot()
         return widget
