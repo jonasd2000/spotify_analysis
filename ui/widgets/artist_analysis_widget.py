c1ef9a5a7406f85ba1f9a3d24e6b872c604b549e
Jonas Dupré <jonasdupre@yahoo.de>
2025-06-23 20:16:20 +0200

    artist_analysis widget displays listening hours over time

--- ui/widgets/artist_analysis_widget.py
+++ ui/widgets/artist_analysis_widget.py
@@ -1,3 +1,4 @@
+import polars as pl
 from nicegui import ui
 
 from .widget import Widget
@@ -6,7 +7,7 @@ from .widget import Widget
 class ArtistAnalysisWidget(Widget):
     top_graph_layouts = {
         "plot_bgcolor": "#E5ECF6",
-        "xaxis": {"fixedrange": True, "showticklabels": False},
+        "xaxis": {"fixedrange": True},
         "yaxis": {
             "fixedrange": True,
             "gridcolor": "white",
@@ -18,7 +19,7 @@ class ArtistAnalysisWidget(Widget):
         "displayModeBar": False,
     }
 
-    selected_artist = None
+    charts = {}
 
     def on_event(self, name, *args, propagate=True, **kwargs):
         match name:
@@ -30,6 +31,7 @@ class ArtistAnalysisWidget(Widget):
 
     def on_data_change(self):
         self.artist_select.set_options(self.get_artist_names())
+        self.update_charts()
 
     def get_artist_names(self):
         if self.data_manager.streaming_data.is_empty():
@@ -41,23 +43,76 @@ class ArtistAnalysisWidget(Widget):
             .to_list()
         )
 
-    def on_artist_change_forward(self, select_artist: str) -> str:
+    def on_artist_change(self, select_artist: str) -> str:
         """
         Called when the artist_select widget is changed.
         Sets the value of self.selected_artist.
         """
+        self.update_charts()
         return select_artist
 
-    def on_artist_change_backward(self, self_artist: str) -> str:
-        """
-        Called when the selected_artist attribute of this widget is changed.
-        Sets the value of the self.artist_select widget.
-        """
-        return self_artist
+    def update_charts(self):
+        new_trace = self.create_artist_over_time_trace()
+        if "artist_over_time" not in self.charts:
+            return
+        self.charts["artist_over_time"]["fig"]["data"][0] = new_trace
+        self.charts["artist_over_time"]["plot"].update()
 
     def create_artist_over_time_trace(self):
         if self.data_manager.streaming_data.is_empty():
             return None
+
+        # time dataframe
+        # a dataframe which contains all year month combinations from the date range of the streaming data
+        min_date = self.data_manager.streaming_data["ts"].min()
+        max_date = self.data_manager.streaming_data["ts"].max()
+        time_df = (
+            pl.DataFrame(
+                {
+                    "date": pl.date_range(
+                        start=min_date,
+                        end=max_date,
+                        interval="1mo",
+                        closed="both",
+                        eager=True,
+                    ),
+                }
+            )
+            .with_columns(
+                pl.col("date").dt.year().alias("year"),
+                pl.col("date").dt.month().alias("month"),
+            )
+            .drop("date")
+        )
+
+        selected_artist = self.artist_select.value
+        data = (
+            self.data_manager.streaming_data.filter(
+                pl.col("master_metadata_album_artist_name") == selected_artist
+            )
+            .group_by(
+                pl.col("ts").dt.year().alias("year"),
+                pl.col("ts").dt.month().alias("month"),
+            )
+            .agg(pl.sum("ms_played"))
+            .sort("year", "month")
+        )
+
+        data = (
+            time_df.join(data, on=["year", "month"], how="left")
+            .fill_null(0)
+            .with_columns(
+                pl.date(pl.col("year"), pl.col("month"), pl.lit(1)).alias("date"),
+            )
+        )
+
+        return {
+            "x": data["date"].to_list(),
+            "y": (data["ms_played"] / 3600000).to_list(),
+            "type": "bar",
+            # "mode": "lines",
+            "name": selected_artist,
+        }
 
     def create_artist_over_time_plot(self):
         trace = self.create_artist_over_time_trace()
@@ -69,9 +124,14 @@ class ArtistAnalysisWidget(Widget):
             "config": self.top_graph_config,
         }
 
-        self.artist_over_time_chart = ui.plotly(fig)
+        artist_over_time_chart = ui.plotly(fig)
 
-        return self.artist_over_time_chart
+        self.charts["artist_over_time"] = {
+            "fig": fig,
+            "plot": artist_over_time_chart,
+        }
+
+        return artist_over_time_chart
 
     def create_widget(self, *args, **kwargs):
         with ui.column() as widget:
@@ -79,11 +139,7 @@ class ArtistAnalysisWidget(Widget):
                 self.get_artist_names(),
                 label="Artist",
                 with_input=True,
-            ).bind_value(
-                self,
-                "selected_artist",
-                forward=self.on_artist_change_forward,
-                backward=self.on_artist_change_backward,
+                on_change=self.on_artist_change,
             )
             with ui.grid(rows=1, columns=r"100%").classes("w-dvw"):
                 self.create_artist_over_time_plot()
