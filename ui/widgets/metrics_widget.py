fc28d1ea8d3fe5e1ea7c93e83cb25d26d9cf2b96
Jonas Dupre <jonasdupre@yahoo.de>
2025-07-21 13:58:16 +0200

    diversity plot and metrics widget integration

--- ui/widgets/metrics_widget.py
+++ ui/widgets/metrics_widget.py
@@ -1,4 +1,25 @@
+from nicegui import ui
+
 from .widget import DataWidget
+from .plots import Plot
 
 class MetricsWidget(DataWidget):
-    pass
+    def create_widget(self, *args, **kwargs):
+        with ui.column() as widget:
+            diversity_plot = Plot(
+                trace={"x": [1, 2, 3], "y": [2, 4, 5], "type": "line", "name": "diversity"},
+                layout={
+                    "plot_bgcolor": "#E5ECF6",
+                    "xaxis": {"fixedrange": True},
+                    "yaxis": {
+                        "fixedrange": True,
+                        "gridcolor": "white",
+                        "title": {"text": "Hours Played"},
+                    },
+                },
+                config={
+                    "responsive": True,
+                    "displayModeBar": False,
+                }, parent=self)
+            diversity_plot.create_widget()
+        return widget
