0fe74e722d5b2c9daf6bfae8be7d96f2d4720159
Jonas Dupre <jonasdupre@yahoo.de>
2026-02-03 15:12:01 +0100

    pipeline module class

--- src/spotify_analysis/data/data_pipeline/pipelines.py
+++ src/spotify_analysis/data/data_pipeline/pipelines.py
@@ -25,69 +25,38 @@ class QueuePair:
         yield self.output_queue
 
 
-class DataPipeline[R, G, P, T]:
-    getter: Getter[R, G]
-    parser: Parser[G, P]
-    transformer: DataTransformer[P, T]
-    loader: Loader[T]
+class PipelineModule:
+    stages: list[AsyncPipelineStage]
+    stage_queues: dict[AsyncPipelineStage, QueuePair]
+    hooks: dict[AsyncPipelineStage, list[asyncio.Queue]]
     
-    hooks: dict[str, list[asyncio.Queue]]
-    
-    def __init__(self, getter: Getter[R, G], parser: Parser[G, P], transformer: DataTransformer[P, T], loader: Loader[T]):
-        self.getter = getter
-        self.parser = parser
-        self.transformer = transformer
-        self.loader = loader
-        
-        got_queue = asyncio.Queue[G]()
-        parsed_queue = asyncio.Queue[P]()
-        transformed_queue = asyncio.Queue[T]()
-        loaded_queue = asyncio.Queue[None]()
-        
-        self.stages: dict[AsyncPipelineStage, QueuePair] = {
-            self.getter: QueuePair(None, got_queue),
-            self.parser: QueuePair(got_queue, parsed_queue),
-            self.transformer: QueuePair(parsed_queue, transformed_queue),
-            self.loader: QueuePair(transformed_queue, loaded_queue)
-        }
-        
+    def __init__(self, stages: list[AsyncPipelineStage]):
+        self.stages = stages
+        self.stage_queues = self._setup_stage_queues()
         self.hooks = {}
-        self.hook_forwards = {}
     
-    def set_input_queue[I, O](self, stage: AsyncPipelineStage[I, O], queue: asyncio.Queue[I]):
-        self.stages[stage].input_queue = queue
-        
-    def get_input_queue(self, stage: AsyncPipelineStage):
-        return self.stages[stage].input_queue
-        
-    def set_output_queue[I, O](self, stage: AsyncPipelineStage[I, O], queue: asyncio.Queue[O]):
-        self.stages[stage].output_queue = queue
-        
-    def get_output_queue(self, stage: AsyncPipelineStage):
-        return self.stages[stage].output_queue
+    def _setup_stage_queues(self) -> dict[AsyncPipelineStage, QueuePair]:
+        self.stage_queues = {QueuePair(None, None) for _ in self.stages}
+        for i, (stage, next_stage) in enumerate(pairwise(self.stages)):
+            stage_out_queue = asyncio.Queue()
+            self._set_output_queue(stage, stage_out_queue)
+            self._set_input_queue(next_stage, stage_out_queue)
+
+        return self.stage_queues            
     
-    def get_stage(self, stage: Literal["getter", "parser", "transformer", "loader"] | AsyncPipelineStage) -> AsyncPipelineStage:
-        if stage is self.getter:
-            return self.getter
-        if stage is self.parser:
-            return self.parser
-        if stage is self.transformer:
-            return self.transformer
-        if stage is self.loader:
-            return self.loader
-        match stage:
-            case "getter":
-                return self.getter
-            case "parser":
-                return self.parser
-            case "transformer":
-                return self.transformer
-            case "loader":
-                return self.loader
-            case _:
-                raise ValueError(f"Unknown stage: {stage}")
+    def _set_input_queue[I, O](self, stage: AsyncPipelineStage[I, O], queue: asyncio.Queue[I]):
+        self.stage_queues[stage].input_queue = queue
+        
+    def _get_input_queue(self, stage: AsyncPipelineStage):
+        return self.stage_queues[stage].input_queue
+        
+    def _set_output_queue[I, O](self, stage: AsyncPipelineStage[I, O], queue: asyncio.Queue[O]):
+        self.stage_queues[stage].output_queue = queue
+        
+    def _get_output_queue(self, stage: AsyncPipelineStage):
+        return self.stage_queues[stage].output_queue
     
-    def register_hook[I, O, Q](self, on: Literal["getter", "parser", "transformer"] | AsyncPipelineStage[I, O], queue: asyncio.Queue[Q], forward: Callable[[O], Q]=None):
+    def register_hook(self, on_index: int, queue: asyncio.Queue):
         """
         Registers a hook for a given stage in the pipeline.
 
@@ -98,13 +67,11 @@ class DataPipeline[R, G, P, T]:
         The hook will be executed in the order in which it was registered.
         """
         
-        forward = forward or (lambda x: x)
-        stage = self.get_stage(on)
+        stage = self.stages[on_index]
         
         if stage not in self.hooks:
             self.hooks[stage] = []
         self.hooks[stage].append(queue)
-        self.hook_forwards[(stage, queue)] = forward
     
     def _create_hooks(self, tg: asyncio.TaskGroup) -> None:
         """
@@ -118,37 +85,66 @@ class DataPipeline[R, G, P, T]:
             A tuple containing the queue to which the hook should write its output, an optional Worker object for the hook, and an optional list of queues to which the hook should write its output.
         """
         
-        for stage, next_stage in pairwise(self.stages): # does not include the last stage (loader) as stage, which i am currently fine with
+        for stage, next_stage in pairwise(self.stages): # does not include the last stage as stage, which i am currently fine with
             if stage not in self.hooks:
                 continue
-            stage_output_queue = self.get_output_queue(stage)
+            stage_output_queue = self._get_output_queue(stage)
             
             hook_worker = Worker(stage_output_queue, 10000, strict=False, batch_processor=queue_splitter)
             
             next_stage_input_queue = asyncio.Queue()
-            self.set_input_queue(next_stage, next_stage_input_queue)
+            self._set_input_queue(next_stage, next_stage_input_queue)
             
             self.register_hook(stage, next_stage_input_queue)
             
-            hooks = {
-                queue: self.hook_forwards[(stage, queue)] for queue in self.hooks[stage]
-            }
-            
-            tg.create_task(self._dispatch_hook_worker(hook_worker, hooks))
+            tg.create_task(self._dispatch_hook_worker(hook_worker, self.hooks[stage]))
     
-    async def _dispatch_hook_worker(self, worker: Worker, hooks: dict[asyncio.Queue, Callable]):
+    async def _dispatch_hook_worker(self, queue_splitter_worker: Worker, hooks: list[asyncio.Queue]):
         async with asyncio.TaskGroup() as tg:
-            tg.create_task(worker(hooks))
+            tg.create_task(queue_splitter_worker(hooks))
         for queue in hooks:
             queue.shutdown()
     
-    async def run(self, input_queue: asyncio.Queue[R]):
+    async def run(self, input_queue: asyncio.Queue, output_queue: asyncio.Queue):
         logger.debug("Running pipeline...")
-        self.set_input_queue(self.getter, input_queue)
+        self._set_input_queue(self.stages[0], input_queue)
+        self._set_output_queue(self.stages[-1], output_queue)
         
         async with asyncio.TaskGroup() as tg:
             self._create_hooks(tg)
-                
-            for stage, (in_queue, out_queue) in self.stages.items():
+            
+            for stage in self.stages:
+                in_queue, out_queue = self.stage_queues[stage]
                 logger.debug(f"Running {stage.__class__.__name__}...")
                 tg.create_task(stage.run(in_queue, out_queue), name=f"pipeline-{stage.__class__.__name__}")
+
+class DataPipeline:
+    modules: list[PipelineModule]
+    
+    module_input_queues: dict[PipelineModule, asyncio.Queue]
+    
+    def __init__(self, main_module: PipelineModule):
+        self.modules = [main_module]
+        self.module_input_queues = {main_module: None}
+    
+    @property
+    def main_module(self):
+        return self.modules[0]
+    
+    def _set_module_input_queue(self, module: PipelineModule, queue: asyncio.Queue):
+        self.module_input_queues[module] = queue
+    
+    def add_module(self, on: tuple[PipelineModule, int], module: PipelineModule):
+        on_module, module_stage_index = on
+        
+        module_in_queue = asyncio.Queue()
+        self.module_input_queues[module] = module_in_queue
+        
+        self.modules.append(module)
+        on_module.register_hook(module_stage_index, module_in_queue)
+        
+    async def run(self, input_queue: asyncio.Queue):
+        self._set_module_input_queue(self.main_module, input_queue)
+        async with asyncio.TaskGroup() as tg:
+            for module in self.modules:
+                tg.create_task(module.run(self.module_input_queues[module], asyncio.Queue()))
